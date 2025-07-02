ecd1342c27cf2fac958b5b127527f39984239c8e
Anastasia Kaloudi <ciaskious@gmail.com>
2025-07-02 20:52:07 +0200

    refactored seed file and added cloudinary

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -1,10 +1,28 @@
 class VansController < ApplicationController
+  before_action :set_van, only: [:show, :edit, :update]
+
   def index
     @vans = Van.all
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @van.update(van_params)
+    redirect_to van_path(@van), notice: "Van was successfully updated."
+  end
+
+  private
+
+  def set_van
     @van = Van.find(params[:id])
   end
 
+  def van_params
+    params.require(:van).permit(:title, :description, :price, :model, :seats, :size, photos: [])
+  end
 end
