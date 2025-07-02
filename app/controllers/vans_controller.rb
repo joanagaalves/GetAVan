8cea42722971c5d30970a497cb6564c2f84e66f7
ROBIN DU APRC <ROBIN@PRIMES-SERVICES.BE>
2025-07-02 22:15:16 +0200

    adding destroy action to vans

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -7,4 +7,9 @@ class VansController < ApplicationController
     @van = Van.find(params[:id])
   end
 
+  def destroy
+    @van = Van.find(params[:id])
+    @van.destroy
+    redirect_to vans_path, notice: "Le van a bien été supprimé."
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :vans, only: [:index, :show]
+  resources :vans, only: [:index, :show, :destroy]
 end
