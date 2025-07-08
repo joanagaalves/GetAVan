4af742e4bbdb2f04282474df536289d99dab2541
Anastasia Kaloudi <ciaskious@gmail.com>
2025-07-08 18:11:09 +0200

    created success page when reservation is completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  get "bookings/success", to: "bookings#success", as: "booking_success"
+
   get "/ui_kit", to: "pages#ui_kit"
   resources :vans do
     resources :bookings, only: [:new, :create]
